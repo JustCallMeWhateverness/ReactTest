cd6001f55874b190a87d45fda17cd6b9e9db3976
Nicole Fritz <nicolefritz00@gmail.com>
2025-09-09 15:31:13 +0200

    Css stuff again

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Row, Col } from 'react-bootstrap';
+
 export default function App() {
   return <>
-    <h1>Hello World!</h1>
+    <header className="container-fluid bg-primary text-white p-3 mb-3">
+      <Row>
+        <Col>
+          <h1>Bootstrap Test</h1>
+        </Col>
+      </Row>
+      <main className="container "> </main>
+      <footer className="container-fluid bg-primary text-white p-3 mt-3">
+        <Row>
+          <Col>
+            <p>Footer Text</p>
+          </Col>
+        </Row>
+      </footer>
+    </header>
   </>;
 }
