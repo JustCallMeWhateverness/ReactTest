23ade0c4e897d6e56def646ad7be312ceabefcef
Nicole Fritz <nicolefritz00@gmail.com>
2025-09-09 14:28:47 +0200

    Sass

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './css/index.css';
+import './sass/index.scss';
 import App from './App.tsx';
 
 createRoot(document.querySelector('#root')!).render(
