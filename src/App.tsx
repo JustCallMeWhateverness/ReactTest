bef4e1ee117242e47ceaa93436324808ad5b9135
Nicole Fritz <nicolefritz00@gmail.com>
2025-09-09 11:45:29 +0200

    Empty for testfetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
-import { useState } from "react";
-import Greeter from "./Greeter";
-import Input from "./Input";
-
 export default function App() {
-  const [greeting, setGreeting] = useState("Hi");
-  const [names, setNames] = useState("World");
-
-  return (
-    <>
-      <Input {...{ names, setNames }} />{" "}
-
-      <Greeter {...{ greeting, setGreeting, names }} />{" "}
-    </>
-  );
+  return <>
+    <h1>Hello World!</h1>
+  </>;
 }
--- src/Input.tsx
+++
@@ -1,7 +0,0 @@
-export default function Input(
-  { names, setNames }:
-    { names: string; setNames: Function; }
-) {
-  return <input value={names}
-    onChange={event => setNames(event.target.value)} />;
-}
