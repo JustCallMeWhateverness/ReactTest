e7070aac677a6dea6e9229414c7886d80fae77c3
Nicole Fritz <nicolefritz00@gmail.com>
2025-09-09 11:46:20 +0200

    FetchTest things

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import useFetchJson from './utils/useFetchJson';
+import Animal from './Animal';
+import Note from './Note';
+
+export interface AnimalData {
+  species: string;
+  description: string;
+}
+
 export default function App() {
-  return <>
-    <h1>Hello World!</h1>
+
+  // Fetch the animals from an url returning json
+  const animals = useFetchJson<AnimalData[]>('/json/animals.json');
+  
+  return animals && <>
+    <h1>Animals I like...</h1>
+    <Note />
+    {animals
+      .filter(({ species }) => species !== 'snake')
+      .sort((a, b) => a.species > b.species ? 1 : -1)
+      .map((props, i) => <Animal key={i} {...props} />)
+    }
   </>;
 }
