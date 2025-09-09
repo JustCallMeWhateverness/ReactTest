cc5c4e01a449a5d90cfeafcf4f2c4a992459ca2f
Nicole Fritz <nicolefritz00@gmail.com>
2025-09-09 14:39:43 +0200

    Boots thingy

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './sass/index.scss';
+import '../sass/index.scss';
 import App from './App.tsx';
 
 createRoot(document.querySelector('#root')!).render(
