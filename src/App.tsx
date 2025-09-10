ac2c4c5b802fb96c6787f3d78a0b855c7f4cb0be
Nicole Fritz <nicolefritz00@gmail.com>
2025-09-10 09:34:36 +0200

    Just fixed things

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 import { Row, Col } from 'react-bootstrap';
 
 export default function App() {
+
+  const headers = [
+    'Header 1',
+    'Header 2',
+    'Header 3',
+    'Header 4',
+    'Header 5',
+    'Header 6',
+    'Header 7',
+    'Header 8',
+    'Header 9',
+  ];
+
   return <>
     <header className="container-fluid bg-primary text-white p-3 mb-3">
       <Row>
@@ -8,8 +21,15 @@ export default function App() {
           <h1>Bootstrap Test</h1>
         </Col>
       </Row>
-      <main className="container "> </main>
-      <footer className="container-fluid bg-primary text-white p-3 mt-3">
+      <main className="container mt-4">
+        <Row>
+          {headers.map(header => <article className="col-sm-6 col-md-4 col-lg-3">
+            <h3>{header}</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut ducimus dolore atque neque animi labore ipsam, odio quae dignissimos, repudiandae deleniti ullam eos aliquam voluptates ad, culpa consequuntur possimus assumenda.</p>
+          </article>)}
+        </Row>
+      </main>
+      <footer className="container-fluid fixed-bottom bg-primary text-white p-3 mt-3">
         <Row>
           <Col>
             <p>Footer Text</p>
